85236365f9ee3f37473cb53840ce3eed8cd8209b
Ryan Park <ryan.c.park44@gmail.com>
2024-04-14 15:32:28 -0400

    Added redirect for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
+import { getSession } from "../lib";
 
-export default function Home() {
-  return (
-    <>
-      <h1>You are now on the home page</h1>
-    </>
-  );
+export default async function Home() {
+  const session = await getSession();
+  if (!session) redirect("/login");
+  else redirect(`/user/${session.userData.athlete.id}?first_name=${session.userData.athlete.firstname}`);
 }
