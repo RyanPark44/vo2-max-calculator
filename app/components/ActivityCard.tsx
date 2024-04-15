ea1dfbcc2c74948df63d6dc040925660ac56a784
Ryan Park <ryan.c.park44@gmail.com>
2024-04-15 07:53:30 -0400

    Added function to lib for fetching athlete activities. Created an ActivityCard component to display activities

--- app/components/ActivityCard.tsx
+++ app/components/ActivityCard.tsx
@@ -1,11 +1,15 @@
+"use client";
 import React from "react";
 
 interface ActivityCardProps {
   title: string;
   date: string;
-  duration: string;
-  distance: string;
+  duration: number;
+  distance: number;
 }
+const handleClick = () => {
+  console.log("clicked");
+};
 const dateOptions: {} = {
   weekday: "long",
   year: "numeric",
@@ -13,16 +17,27 @@ const dateOptions: {} = {
   day: "numeric",
 };
 const ActivityCard = (activityData: ActivityCardProps) => {
+  const date = new Date(activityData.date).toLocaleDateString(
+    "en-us",
+    dateOptions,
+  );
+  const hours = Math.floor(activityData.duration / 3600);
+  const minutes = Math.floor(activityData.duration / 60 - hours * 60);
+  const seconds = Math.floor(activityData.duration % 60);
+  const distance = (activityData.distance / 1000).toFixed(2); // convert meters to kilometers
   return (
-    <div className="card w-1/3 m-5 bg-sky-400 text-primary-content hover:bg-sky-500">
+    <div
+      className="card w-1/3 m-5 bg-sky-400 text-primary-content hover:bg-sky-500"
+      onClick={handleClick}
+    >
       <div className="card-body">
         <h2 className="card-title">{activityData.title}</h2>
+        <p>{date}</p>
         <p>
-          Date:{" "}
-          {new Date(activityData.date).toLocaleDateString("en-us", dateOptions)}
+          Duration: {hours > 0 ? `${hours}h` : ""}{" "}
+          {minutes > 0 ? `${minutes}m` : ""} {seconds}s
         </p>
-        <p>Duration: {activityData.duration}</p>
-        <p>Distance: {activityData.distance}</p>
+        <p>Distance: {distance} km</p>
       </div>
     </div>
   );
